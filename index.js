b40b9e5b86dd5e6431f021a1f81e88c88aa61c66
Kevin Thizy <kevinthizy@MBPro-Kevin.local>
2018-09-21 08:32:58 +0200

    Commander.

--- index.js
+++ index.js
@@ -1,174 +1,28 @@
-const _ = require('lodash');
-const yaml = require('js-yaml');
-const fs = require('fs');
+#!/usr/bin/env node
+
+const program = require('commander');
 const path = require('path');
 
-module.exports = {
-    generate,
-};
+const generator = require('./lib');
 
-async function generate({ folder }) {
-    console.log('generate doc from folder: ' + folder);
+program
+    .version('0.1.0')
+    .option('-i, --in [api-folder]', 'Path to project folder containing serverless.yml definition.')
+    .option('-o --out [out-folder]', 'Path to desired out folder.')
+    .parse(process.argv);
 
-    const apiPath = path.join(__dirname, folder);
-    const serverlessFilePath = path.join(apiPath, './serverless.yml');
-    
-    try {
-        var doc = loadServerlessDefinition(serverlessFilePath);
-        
-        const handlers = getRoutes(doc);
-
-        const docs = await loadDocsFromHandlers(apiPath, handlers);
-        
-        const routes = groupByRoute(handlers, docs);
-
-        const yml = createDoc(routes);
-
-        saveApiDocFile(yml);
-    } catch (e) {
-        console.log(e);
-    }
+if (!program.in) {
+    throw new Error('--in [api-folder] in required.');
 }
 
-function loadServerlessDefinition(pathToFile) {
-    return yaml.safeLoad(fs.readFileSync(pathToFile, 'utf8'));
+if (!program.out) {
+    throw new Error('--out [out-folder] in required.');
 }
 
-function getRoutes(serverless) {
-    return Object.values(serverless.functions)
-        .filter(func => Array.isArray(func.events))
-        .map(func => {
-            const handler = func.handler;
+const folder = path.join(__dirname, program.in);
+const out = path.join(__dirname, program.out);
 
-            const routes = func.events.filter(event => event.http).map(route => {
-                const { method, path } = route.http;
-                return { method, path };
-            });
-
-            return { routes, handler };
-        });
-}
-
-async function loadDocsFromHandlers(apiPath, handlers) {
-    const handlerList = handlers.map(handler => handler.handler);
-    const fileList = _.uniq(handlerList.map(handler => [handler, path.join(apiPath, handler.split('.')[0])]));
-    
-    const docRegex = /(\/\*\*[\s\S]*?\*\/)\nmodule\.exports\.(.+) = async \(.+\) => {[\s\S]*?}/g;
-    const apiDocRegex = /@apidoc \[([\s\S]*)\]/g;
-
-    const rawDocs = _.compact(await Promise.all(fileList.map(async ([handler, filePath]) => {
-        const file = fs.readFileSync(filePath + '.js', 'utf-8');
-
-        const result = docRegex.exec(file);
-        if (!result) {
-            return;
-        }
-
-        const [matched, ...groups] = [...result];
-
-        if (groups.length % 2 === 1) {
-            return;
-        }
-
-        const handlerDoc = {};
-        while (groups.length > 0) {
-            const [doc, method] = groups.splice(0, 2);
-
-            const result = apiDocRegex.exec(doc);
-            if (result) {
-                let [matched, parsedDoc] = [...result];
-                parsedDoc = parsedDoc.replace(/ \*/g, "");
-                const ymlDoc = yaml.safeLoad(parsedDoc);
-
-                handlerDoc[method] = ymlDoc;
-            }
-        }
-
-        return [handler, handlerDoc];
-    })));
-
-    return _.fromPairs(rawDocs); 
-}
-
-function groupByRoute(handlers, docs) {
-    const routes = {};
-
-    handlers.forEach(handler => {
-        handler.routes.forEach(route => {
-            if (!routes[route.path]) routes[route.path] = {};
-            
-            const routeDoc = ((docs[handler.handler] || {})[route.method]) || {};
-            routeDoc.description = routeDoc.description || "No description";
-            routeDoc.responses = routeDoc.responses || {};
-            routeDoc.responses.default = routeDoc.responses.default || { '$ref': "#/components/responses/ErrorResponse" };
-            routeDoc.responses['2XX'] = routeDoc.responses['2XX'] || { '$ref': "#/components/responses/GeneralResponse" };
-
-            routes[route.path][route.method] = routeDoc;
-        });
-    });
-
-    return routes;
-}
-
-function createDoc(routes, { title = 'Generated API', description = 'No description', termsOfService = '', contact = {}, version = '1.0.0' } = {}) {
-    const doc = {
-        openapi: "3.0.0",
-        info: {
-            title,
-            description,
-            termsOfService,
-            contact,
-            version,
-        },
-        paths: routes,
-        components: {
-            responses: {
-                GeneralResponse: {
-                    description: 'General Response Object',
-                    content: {
-                        'application/json': {
-                            type: 'object'
-                        },
-                    },
-                },
-                ErrorResponse: {
-                    description: 'Error Object',
-                    content: {
-                        'application/json': {
-                            type: 'object',
-                            properties: {
-                                errorMessage: {
-                                    type: 'string',
-                                    example: "[400] a user_id or user_ids must be provided in body",
-                                },
-                                errorType: {
-                                    type: 'string',
-                                    example: "BadRequestError",
-                                },
-                                stackTrace: {
-                                    type: 'array',
-                                    items: {
-                                        items: {
-                                            type: 'string'
-                                        },
-                                    },
-                                },
-                            }
-                        },
-                    },
-                }
-            }
-        }
-    };
-
-    const yml = yaml.safeDump(doc);
-
-    return yml;
-}
-
-function saveApiDocFile(yml) {
-    fs.writeFileSync('api-doc-generated.yml', yml);
-}
-
-
-generate({ folder: '../api-ico' });
+generator.generate({
+    folder,
+    out,
+});
