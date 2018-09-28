eda6df33adb390ff1a5cae1ebe20bfd757be73c1
THIZY Kevin <kevin.thizy@intech.lu>
2018-09-28 09:12:52 +0200

    Added serverURL and serverName as command params.
    --server-name "name"
    --server-url "url"

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const generator = require('./lib');
 
 program
     .version('0.1.0')
-    .option('-i, --in [api-folder]', 'Path to project folder containing serverless.yml definition.')
+    .option('-i --in [api-folder]', 'Path to project folder containing serverless.yml definition.')
     .option('-o --out [out-folder]', 'Path to desired out folder.')
+    .option('-surl --server-url [server-url]', 'URL of API server.')
+    .option('-s --server-name [server-name]', 'Name of API server.')
     .parse(process.argv);
 
 if (!program.in) {
@@ -25,4 +27,6 @@ const out = path.join(process.cwd(), program.out);
 generator.generate({
     folder,
     out,
+    serverName: program.serverName,
+    serverURL: program.serverUrl,
 });
